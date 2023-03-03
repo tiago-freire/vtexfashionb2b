d7b3624f362344ba8db3993aa9b9ce40183fcc20
Tiago de Andrade Freire <tiago.freire@cubos.io>
2023-03-03 12:34:43 -0300

    feat: theme for global vtex fashion b2b store

--- checkout-ui-custom/checkout6-custom.js
+++ checkout-ui-custom/checkout6-custom.js
@@ -43,7 +43,7 @@
       },
       buildButton: function buildButton() {
         return $(
-          '<button class="jsSubmitFakeButton btn btn-success btn-large btn-block">\n        <i class="icon-lock"></i>\n        <i class="icon-spinner icon-spin" style="display: none;"></i>\n        <span>Completar Pedido</span>\n      </button>'
+          '<button class="jsSubmitFakeButton btn btn-success btn-large btn-block">\n        <i class="icon-lock"></i>\n        <i class="icon-spinner icon-spin" style="display: none;"></i>\n        <span>Place Order</span>\n      </button>'
         );
       },
       hideLoadingAndResetButton: function hideLoadingAndResetButton() {
@@ -80,7 +80,7 @@
       },
       buildInput: function buildInput() {
         return $(
-          '\n        <div style="\n          padding-right: 15px;\n          margin-bottom: 10px;\n        " id="poWrap">\n        <label> Número do Pedido de Compra: (opcional) </label>\n          <input \n            type="text"\n            id="poNumberInput" style="\n            font-size: 20px;\n            height: 36px;\n            line-height: 34px;\n            width: 100%;" \n            name="poNumber">\n          <span class="help error feedback" style="display: none">This field is required.</span>\n        </div>'
+          '\n        <div style="\n          padding-right: 15px;\n          margin-bottom: 10px;\n        " id="poWrap">\n        <label> Purchase Order Number: (optional) </label>\n          <input \n            type="text"\n            id="poNumberInput" style="\n            font-size: 20px;\n            height: 36px;\n            line-height: 34px;\n            width: 100%;" \n            name="poNumber">\n          <span class="help error feedback" style="display: none">This field is required.</span>\n        </div>'
         );
       },
       showError: function showError() {
@@ -179,3 +179,52 @@
     disablePersonalInfoEditButton.init();
   });
 })(window, window.jQuery);
+
+window.addEventListener("DOMContentLoaded", () => {
+  $(window).on(
+      'orderFormUpdated.vtex ' +
+      'renderLoaderReady.vtex ' +
+      'paymentUpdated.vtex ' +
+      'stateUpdated.vtex ' +
+      'deliverySelected.vtex ' +
+      'shippingInformationUpdated.vtex ' +
+      'shippingRatePreviewLoading.vtex ' +
+      'transactionValidation.vtex ' +
+      'omniShippingChanged.vtex ' +
+      'componentValidated.vtex ' +
+      'componentDone.vtex ' +
+      'checkoutRequestBegin.vtex ' +
+      'hashchange', () => {
+
+      if (!$('.demo-store-warning').length) {
+          const demoWarning = '<div class="demo-store-warning">Attention! This is a VTEX platform demo store. Products, prices and deliveries are merely illustrative.</div>';
+          $('.summary-template-holder').append(demoWarning);
+      }
+
+      if (!$('#continue-shopping-button').length) {
+          $('.btn-place-order-wrapper').append($('<a href="/" target="_self" id="continue-shopping-button" class="btn btn-large btn-secondary">Keep buying</a>'));
+      }
+
+      const cartToOrderFormElement = document.getElementById('cart-to-orderform')
+      const htmlCartToOrderForm = `
+          PROCEED
+          <svg width="14" height="12" viewBox="0 0 14 10" fill="none" xmlns="http://www.w3.org/2000/svg">
+              <path d="M12.9142 4.58594L8.62115 0.292942L7.20715 1.70694L10.5002 4.99994H0.500153V6.99994H10.5002L7.20715 10.2929L8.62115 11.7069L12.9142 7.41394C13.2891 7.03889 13.4997 6.53027 13.4997 5.99994C13.4997 5.46961 13.2891 4.961 12.9142 4.58594Z" fill="white"/>
+         </svg>
+      `
+      $(cartToOrderFormElement).html(htmlCartToOrderForm);
+      var observer = new MutationObserver(() => {
+        observer.disconnect();
+        $(cartToOrderFormElement).html(htmlCartToOrderForm);
+      });
+
+      observer.observe(cartToOrderFormElement, {
+        childList: true,
+        attributes: true,
+        characterData: true,
+        subtree: true,
+        attributeOldValue: true,
+        characterDataOldValue: true
+      });
+  });
+});
